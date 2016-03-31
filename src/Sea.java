0d1f4c9c3ec5cf92da57b2047941a0b17d0ef01c
Администратор <@Administrator>
2016-03-31 23:20:20 +0300

    обн.2

--- src/Sea.java
+++ src/Sea.java
@@ -30,14 +30,14 @@ class MyFrame extends JFrame {
 }
 class MyPanel extends JPanel implements ActionListener{	
 	
-	Draw draw = new Draw();
-	Player player = new Player();
+	Draw draw = new Draw();	
 	Rectangle key=new Rectangle(500,50,220,35);	
 	int x,y;//координаты мышки
 	int time;
 	Timer timer=new Timer(1,this);
-	Image im;
-	ArrayList<Rect> rects=new ArrayList<Rect>();
+	
+	ArrayList<Player> players=new ArrayList<Player>();
+	ArrayList<Room> rooms=new ArrayList<Room>();
 	
 	
 	
@@ -45,57 +45,74 @@ class MyPanel extends JPanel implements ActionListener{
 		addMouseMotionListener( new MyMouse());
 		addMouseListener( new MyMouse());
 		timer.start();
-		
-		try{
-			im = ImageIO.read(new File("Image/Sea.jpg"));
-		}catch(IOException exception){}
+		//игроки
+		players.add(new Player(true));
+		players.add(new Player(false));
 		//планки кают
 		for(int i=0;i<3;i++){
-			rects.add(new Rect());
+			rooms.add(new Room(i*530));
 		}
 		
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		
-		time++;
-		if(time>Okean.timer){
-			time=0;
-			Okean.movement();
+		for(Player player:players){
+			
+			time++;
+			if(time>player.timer){
+				time=0;				
+				for(Room room:rooms){
+					if(!player.team)room.y--;
+					//else room.y++;
+				}
+				
+			}
+			
 		}
-		//player.click();
+			//player.click();
 		
 		repaint();	
+	}
+	void movement(){
+		
 	}
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		//фон
 		setBackground(new Color(120,220,255));
-		g.drawImage(im,375,10,null);
+		
 		
 		//g.fillOval(x, y, 10, 10);//
 		
-		g.drawRoundRect(500,50,220,35,10,10);
-		g.drawString("Количество кликов: "+player.getClickCount(), 540, 70);
+		//g.drawRoundRect(500,50,220,35,10,10);
+		//g.drawString("Количество кликов: "+player.getClickCount(), 540, 70);
+		
+		//rooms
+		for(Room room:rooms){
+		g.drawImage(room.image,room.x,room.y,null);		
+		}
 		//вода			
 		g.setColor(new Color(0,95,140));
 		g.fillRect(Okean.x, Okean.y, Okean.width, Okean.height);
 		//стенки
 		g.setColor(Color.black);
-		
+		for(Room room:rooms){			
+			g.fillRect(375, room.y-30, 500, 30);
+		}				
 		g.fillRect(345,0,30,1050);
 		g.fillRect(875,0,30,1050);
 		
-		g.fillRect(355,-20,540,30);
-		g.fillRect(355,510,540,30);
+		
 	}
 	
 	public class MyMouse extends MouseAdapter implements MouseMotionListener{				  				
 		   
 		public void mousePressed(MouseEvent event){					
 				if(key.contains(event.getPoint())){
-					player.click();
+					
+					//player.click();
 					
 				}																	
 		}		
@@ -114,19 +131,25 @@ class MyPanel extends JPanel implements ActionListener{
 }
 class Okean{
 	static int x=375,y=350,width=500,height=1000;
-	static int timer=500;
+	
 	
 	static void movement(){
 		y--;  height++;
 	}
 }
 
-class Rect{
-	int x=355,y=-20,width=540,heigth=30;
+class Room{
+	int x=375,y,width=500,height=500;	
+	Image image;
 	
-	void movement(){
-		y++;
-	}
+	Room(int y){
+		this.y=y;
+		
+		try{
+			image = ImageIO.read(new File("Image/Sea.jpg"));
+		}catch(IOException exception){}
+	}	
+	
 }
 
 /**
@@ -145,6 +168,12 @@ class Draw {
 */
  class Player {
    private int clickCount;
+   boolean team;
+   int timer=100;
+   
+   Player(boolean team){
+	   this.team=team;
+   }
 
    public int getClickCount(){
        return clickCount;
